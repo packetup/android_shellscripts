5e6d8f302e224e0e65bed5e1988c7fa46df319a8
packetup <44579799+packetup@users.noreply.github.com>
2018-11-22 16:34:15 -0500

    Update modify-build-fingerprint script

--- 00002-modify-build-fingerprint.sh
+++ 00002-modify-build-fingerprint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -i 's@$(PRODUCT_BRAND)/$(TARGET_PRODUCT)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/$(BUILD_VERSION_TAGS)@google/marlin/marlin:9/PPR2.181005.003/4984323:user/release-keys@' ${BUILD_DIR}/build/core/Makefile
+sed -i 's@$(PRODUCT_BRAND)/$(TARGET_PRODUCT)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BF_BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/$(BUILD_VERSION_TAGS)@google/marlin/marlin:9/PPR2.181005.003/4984323:user/release-keys@' ${BUILD_DIR}/build/core/Makefile
